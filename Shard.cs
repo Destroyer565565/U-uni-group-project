7f58ea26c7c3f2ca5736e64fc5d6ffb77198c569
Destroyer565565 <108770555+Destroyer565565@users.noreply.github.com>
2022-07-07 14:16:56 +1000

    Update Shard.cs

--- Shard.cs
+++ Shard.cs
@@ -7,13 +7,15 @@ public class Enemy : MonoBehaviour
    [SerializeField] private Rigidbody2D rgbd;
    public float dir;
    public float speed;
-  
+   
    
 
-   
+   void Update() {
+    rgbd.transform.Translate(dir * speed * Time.deltaTime, rgbd.velocity.y,0);
+   }
 
    void OnCollisionEnter2D(Collision2D other) {
       dir = dir * -1;
-   rgbd.transform.Translate(dir * speed * Time.deltaTime, rgbd.velocity.y,0);
+
    }
 }
