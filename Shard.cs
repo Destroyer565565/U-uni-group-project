0a87d1195a7da3723aca3817246ac9e1f4917a43
Destroyer565565 <108770555+Destroyer565565@users.noreply.github.com>
2022-07-07 14:34:48 +1000

    Update Shard.cs

--- Shard.cs
+++ Shard.cs
@@ -4,24 +4,37 @@ using UnityEngine;
 
 public class Shard : MonoBehaviour
 {
-   [SerializeField] private Rigidbody2D rgbd;
-   public float dir;
-   public float speed;
 
+[SerializeField] private Rigidbody2D rgbd;
 
+public float speed
 
-   
-   
+void Update()
+{
+    CheckMove();
+}
 
-   
+private void CheckMove(){
+    float dir = -1f;
 
-   private void Move(float dir)
-    {
-        rgbd.transform.Translate(new Vector3(dir, rgbd.velocity.y,0));
-    }
+    Move(dir * Time.deltaTime * speed);
+}
 
-   void OnCollisionEnter2D(Collision2D other) {
+private void Move(float dir){
+    rgbd.transform.Translate(new Vector3(dir, rgbd.velocity.y,0));
+}
+  void OnCollisionEnter2D(Collision2D other) {
       dir = dir * -1;
 
    }
+
+
+
+
+
+
+
+
+
+
 }
