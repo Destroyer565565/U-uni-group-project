2c93d1bcec2fb13171ddb9916088d5d98795ee33
Destroyer565565 <108770555+Destroyer565565@users.noreply.github.com>
2022-07-07 13:03:13 +1000

    Add files via upload

---
+++ Trash.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trash : MonoBehaviour
+{
+
+ private void OnCollisionEnter2D(Collision2D col){
+
+ if(col.gameObject.tag == "Player") {
+     Destroy(this.gameObject);
+ }
+ 
+}
+
+}
+
+
