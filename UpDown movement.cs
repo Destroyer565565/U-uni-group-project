f78896946ea3a7f1ad67dd3aab219e87ce6c1105
Destroyer565565 <108770555+Destroyer565565@users.noreply.github.com>
2022-07-07 11:04:18 +1000

    Add files via upload

---
+++ UpDown movement.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpDownmovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+   [SerializeField] private Rigidbody2D rgbd;
+
+   [SerializeField] private AudioSource sfx;
+
+    public float speed;  
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckMove(); // A and D
+
+        UpdownM(); // W and S
+    }
+
+    private void CheckMove() // A and D
+    {
+
+        float dir = 0f;
+        if(Input.GetKey(KeyCode.A))
+        {
+            dir = -1f;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            dir = 1f;
+        }
+        
+        if(!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        {
+            dir = 0f;
+        }
+        
+        Move(dir * Time.deltaTime * speed);
+
+    }
+    private void Move(float dir)
+    {
+        rgbd.transform.Translate(new Vector3(dir, rgbd.velocity.y,0));
+    }
+     
+     private void UpdownM() // W and S
+     {
+
+         float UD = 0f; // Up and down
+         if(Input.GetKey(KeyCode.W)) {
+             UD = 1f;
+         }
+         if(Input.GetKey(KeyCode.S)) {
+             UD = -1f;
+         }
+         if(!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+        {
+            UD = 0f; 
+        }
+         WD(dir * Time.deltaTime * speed); // Up And down
+        
+     }
+      
+      private void WD(float UD)
+      {
+          rgbd.transform.Translate(new Vector3(rgbd.velocity.x, UD, 0));
+      }
+     
+     private void OnCollisionEnter2D(Collision2D bonk) //When the player touches the wall they die and reset
+     {
+         
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
