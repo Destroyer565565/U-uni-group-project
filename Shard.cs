3a5139ab4602385fd7cbd13eea3dfe31508803b7
Destroyer565565 <108770555+Destroyer565565@users.noreply.github.com>
2022-07-08 10:06:05 +1000

    Update Shard.cs

--- Shard.cs
+++ Shard.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement
+using UnityEngine.SceneManagement;
 
 public class Shard : MonoBehaviour
 {
@@ -13,7 +13,7 @@ public class Shard : MonoBehaviour
        if(col.gameObject.tag == "Player")
        {
            
-           SceneManager.Loadscene(1)
+           SceneManager.Loadscene(1);
 
        }
    }
