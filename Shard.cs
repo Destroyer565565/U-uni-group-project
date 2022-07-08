ffa8cadcdedd34230d20241880f9df5c2fb8aa0b
Destroyer565565 <108770555+Destroyer565565@users.noreply.github.com>
2022-07-08 10:02:13 +1000

    Update Shard.cs

--- Shard.cs
+++ Shard.cs
@@ -1,23 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement
 
-public class Enemy : MonoBehaviour
+public class Shard : MonoBehaviour
 {
    [SerializeField] private Rigidbody2D rgbd;
    public float dir;
    public float speed;
    void OnTriggerEnter2D(Collider2D col) {
 
-       if(col.gameObject.tag == "Main")
+       if(col.gameObject.tag == "Player")
        {
-           Destroy(this.gameObject);
+           
+           SceneManager.Loadscene(1)
+
        }
    }
    
 
    void Update() {
-    rgbd.transform.Translate(rgbd.velocity.x * dir * speed * Time.deltaTime,0);
+    rgbd.transform.Translate(dir * speed * Time.deltaTime, rgbd.velocity.y,0);
    }
 
    void OnCollisionEnter2D(Collision2D other) {
