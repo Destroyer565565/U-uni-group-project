adb9fb173071e588c264d038c1400ded6f0cda7b
Destroyer565565 <108770555+Destroyer565565@users.noreply.github.com>
2022-07-08 10:33:27 +1000

    Update Checkpoint 1.cs

--- Checkpoint 1.cs
+++ Checkpoint 1.cs
@@ -1,3 +1,7 @@
+// SceneB.
+// SceneB is given the sceneBuildIndex of 0 which will
+// load SceneA from the Build Settings
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,12 +9,13 @@ public class Checkpoint1 : MonoBehaviour
 {
     void Start()
     {
-    
+        Debug.Log("LoadSceneB");
     }
 
     public void LoadB(int sceneANumber)
     {
-        
+        if(gameObject.tag == "Player") {
         SceneManager.LoadScene(1);
+        }
     }
 }
